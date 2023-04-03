88e0859bd394e638732aec0859faf8f12205c043
PavlaSonnenschein <psonnenschein@seznam.cz>
2023-04-03 20:36:48 +0200

    Úkol 1

--- ukol-1.py
+++ ukol-1.py
@@ -3,17 +3,17 @@ jmeno = ("Pavla")
 print(f"{jmeno}@czechitas.cz")
 
 jmeno_a_prijmeni = input("Jméno a příjmení: ")
+rozdeleni = jmeno_a_prijmeni.split()
 
 print(jmeno_a_prijmeni .upper())
 print(jmeno_a_prijmeni .lower())
 
-print(jmeno_a_prijmeni[0].upper() + jmeno_a_prijmeni[1:5+1] + jmeno_a_prijmeni[6].upper() + jmeno_a_prijmeni[7:])
+print(rozdeleni[0][0].upper() + rozdeleni[0][1:] + " " + rozdeleni[1][0].upper() + rozdeleni[1][1:])
 
-print(jmeno_a_prijmeni[0].upper() + ". " + jmeno_a_prijmeni[6].upper() + ".")
+print(rozdeleni[0][0].upper() + ". " + rozdeleni[1][0].upper() + ".")
 
-rozdeleni = jmeno_a_prijmeni.split()
 if len(rozdeleni[0]) > 5:
-    print(rozdeleni[0][0].upper() + rozdeleni[1][0].upper() + rozdeleni[1][1:])
+    print(rozdeleni[0][0].upper() + " "+ rozdeleni[1][0].upper() + rozdeleni[1][1:])
 else:
     print(jmeno_a_prijmeni)
 
