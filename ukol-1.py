9761348ec0feaf2ba3287cbe82b2e299d431d016
PavlaSonnenschein <psonnenschein@seznam.cz>
2023-04-03 20:43:20 +0200

    Úkol-1

--- ukol-1.py
+++ ukol-1.py
@@ -8,13 +8,13 @@ rozdeleni = jmeno_a_prijmeni.split()
 print(jmeno_a_prijmeni .upper())
 print(jmeno_a_prijmeni .lower())
 
-print(rozdeleni[0][0].upper() + rozdeleni[0][1:] + " " + rozdeleni[1][0].upper() + rozdeleni[1][1:])
+print(rozdeleni[0][0].upper() + rozdeleni[0][1:].lower() + " " + rozdeleni[1][0].upper() + rozdeleni[1][1:].lower())
 
 print(rozdeleni[0][0].upper() + ". " + rozdeleni[1][0].upper() + ".")
 
 if len(rozdeleni[0]) > 5:
-    print(rozdeleni[0][0].upper() + " "+ rozdeleni[1][0].upper() + rozdeleni[1][1:])
+    print(rozdeleni[0][0].upper() + " "+ rozdeleni[1][0].upper() + rozdeleni[1][1:].lower())
 else:
-    print(jmeno_a_prijmeni)
+    print(rozdeleni[0][0].upper() + rozdeleni[0][1:].lower() + " " + rozdeleni[1][0].upper() + rozdeleni[1][1:].lower())
 
 print(jmeno_a_prijmeni[0:1] + jmeno_a_prijmeni[1].upper() + jmeno_a_prijmeni[2:8] + jmeno_a_prijmeni[8:10].upper() + jmeno_a_prijmeni[10:])
